2a453c1200c6db1f06fa05efb021359df2f97617
zaoqi <zaomir@outlook.com>
2018-06-15 15:26:36 +0800

    [WIP]make-html.rkt

--- make-html.rkt
+++ make-html.rkt
@@ -1,13 +1,15 @@
 #lang racket
-{require html}
+{require html-parsing}
 {require xml}
 {require racket/system}
 {define paths (string-split (port->string (first (process "find -wholename './blog-cn/*/*/*/*' | sort"))) "\n")}
 {define all-bodys '()}
 {for ([path paths])
   {define port (open-input-file	path)}
-  {define htmlv (read-html port)}
-  {match-define (html _ (list _ (body _ bodys))) htmlv}
-  {set! all-bodys (append bodys all-bodys)}
+  {define htmlv (html->xexp port)}
+  {match-define (list '*TOP* _ ... (list 'html _ ... (list 'body body ...) _ ...) _ ...) htmlv}
+  {writeln body}
+  (error)
   }
-{define all-html (html '() (list (head '() '()) (body '() all-bodys)))}
+;{define all-html (html '() (list (head '() '()) (body '() all-bodys)))}
+;(write-xml all-html)
